fe8c3922b999bddf92bc4b704044517197cdbc66
meadej <meade.jonathan@gmail.com>
2018-03-05 00:31:03 -0700

    Added a data runner

--- data_fetcher.py
+++ data_fetcher.py
@@ -15,3 +15,16 @@ class data_fetcher:
             return object
         except:
             raise IOError("Unable to fetch object at " + str(url))
+
+    def get_list_objects(self, url_list):
+        return_array = []
+        try:
+            for url in url_list:
+                return_array.append(self.get_object_from_url(url))
+        except:
+            raise Exception("Failed to get objects in list")
+        return return_array
+
+    def store_objects_locally(self, object_list, local_destination):
+        for object in object_list:
+            self.store_object_locally(object, local_destination)
---
+++ data_runner.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from datetime import timedelta
+from data_fetcher import data_fetcher
+
+def get_last_sixty_days():
+    numdays = 60
+    base = datetime.today()
+    date_list = [base - timedelta(days=x) for x in range(0, numdays)]
+    return date_list
+
+def format_date_string(input_date, format_string):
+    return input_date.strftime(format_string)
+
+def main():
+    pdf_fetcher = data_fetcher()
+    date_list = get_last_sixty_days()
+    url_list = []
+    for date in date_list:
+        url_list.append("https://www.colorado.edu/police/blotter/" + str(format_date_string(date,"%Y%m%d")) + ".pdf")
+    for url in url_list:
+        pdf_list = pdf_fetcher.get_list_objects(url_list)
+        pdf_fetcher.store_objects_locally(pdf_list, "pdfs/")
+    return
+
+main()
