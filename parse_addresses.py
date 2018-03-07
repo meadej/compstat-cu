9d84f4e21ef08ecddb3dc28d29fe2ba83659930e
BuildTools <meade.jonathan@gmail.com>
2018-03-07 13:19:27 -0700

    Minor changes

--- parse_addresses.py
+++ parse_addresses.py
@@ -18,7 +18,7 @@ def parse_address_from_crime_string(cr_string):
     second_string = re.split('[0-9]{4}-[0-9]{8}', cr_string)[1]
     address_suffix = re.split('8[0-9]{4}', second_string)[0]
     address_arr = address_suffix.split(" ")
-    address = address_arr[2:len(address_arr) - 1]
+    address = address_arr[3:len(address_arr) - 1]
     return address
 
 absolute_dir = "/home/jon/code/cu-blotter/pdfs/"
@@ -35,4 +35,5 @@ for filename in files:
         for cr_string in crime_strings:
             address_arr = parse_address_from_crime_string(cr_string)
             address = " ".join(address_arr)
-            print(address)
+            if "Boulder" in address:
+                print(address)
