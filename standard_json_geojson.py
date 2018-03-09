6baabdf385bf0ac9b2d4bd6679c79acf015caf1e
meadej <meade.jonathan@gmail.com>
2018-03-08 20:56:04 -0700

    GeoJSON updates

--- standard_json_geojson.py
+++ standard_json_geojson.py
@@ -19,8 +19,32 @@ def convert_to_geojson(s_json):
     ret_json = json.dumps(ret_dict)
     return ret_json
 
-data_file = "/crime_db.json"
-for line in open(os.getcwd() + data_file).readlines():
+def write_geojson_header_to_file(file_location):
+    f_handle = open(file_location, 'w')
+    opening_json_string = "{\"type\": \"FeatureCollection\",\"features\": ["
+    f_handle.write(opening_json_string)
+    f_handle.close()
+
+def write_geojson_data_to_file(file_location, data):
+    f_handle = open(file_location, 'a')
+    f_handle.write(data + "\n")
+    f_handle.close()
+
+def write_geojson_closer_to_file(file_location):
+    f_handle = open(file_location, 'a')
+    closing_json_string = "]}"
+    f_handle.write(closing_json_string)
+    f_handle.close()
+
+data_file = os.getcwd() + "/crime_db.json"
+out_file = os.getcwd() + "/crime_db.geojson"
+
+write_geojson_header_to_file(out_file)
+
+for line in open(data_file).readlines():
     j_data = read_standard_json(line)
     ret_data = convert_to_geojson(j_data)
-    print(ret_data)
+    write_geojson_data_to_file(ret_data)
+
+write_geojson_closer_to_file(out_file)
+
