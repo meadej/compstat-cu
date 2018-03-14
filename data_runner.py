4cd2251129d35cbaffe3bd4af86a974b6feab005
BuildTools <meade.jonathan@gmail.com>
2018-03-14 15:23:29 -0600

    Updates

--- data_runner.py
+++ data_runner.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from datetime import timedelta
 from data_fetcher import data_fetcher
 import os
+import sys
 
 def get_last_n_days(n):
     numdays = n
@@ -13,6 +14,10 @@ def format_date_string(input_date, format_string):
     return input_date.strftime(format_string)
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python data_runner.py [output folder]"
+	exit(1)
+    out_folder_name = str(sys.argv[1])
     pdf_fetcher = data_fetcher()
     date_list = get_last_n_days(50)
     url_list = []
@@ -24,7 +29,7 @@ def main():
     title_list = []
     for i in range(0, len(date_list)):
         title_list.append(str(formatted_date_list[i]) + ".pdf")
-    pdf_fetcher.store_objects_locally(title_list, pdf_list, os.getcwd() + "/pdfs/")
+    pdf_fetcher.store_objects_locally(title_list, pdf_list, os.getcwd() + "/" + out_folder_name + "/")
     return
 
 main()
--- lookup_addresses.py
+++ lookup_addresses.py
@@ -2,6 +2,7 @@ from geocoding_interface import geocoding_interface
 import os
 import time
 import json
+import sys
 
 def lookup_address(address_string, gi):
     try:
@@ -19,12 +20,18 @@ def lookup_address(address_string, gi):
         except:
             return None
 
+if len(sys.argv) < 3:
+    print("Usage: python lookup_addresses.py [input file] [output file]")
+    exit(1)
 
 address_arr = []
 geo_int = geocoding_interface()
-data_file = "/crime_db.json"
+data_file = sys.argv[1]
+out_file = sys.argv[2]
 
-f_handle = open(os.getcwd() + data_file)
+f_handle = open(os.getcwd() + "/" + data_file, 'r')
+o_handle = open(os.getcwd() + "/" + out_file, 'w')
+
 for line in f_handle.readlines():
     j_line_data = json.loads(line)
     case_number = list(j_line_data.keys())[0]
@@ -34,4 +41,7 @@ for line in f_handle.readlines():
         j_line_data[case_number]["coordinates"]["lat"] = float(lat_lon[0])
         j_line_data[case_number]["coordinates"]["lon"] = float(lat_lon[1])
         j_return_data = json.dumps(j_line_data)
-        print(j_return_data)
+        o_handle.write(j_return_data)
+
+f_handle.close()
+o_handle.close()
